b30a9e5515c097abf37ba423d3b19201e58a46c9
Henry <henryma14@gmail.com>
2016-10-28 14:11:13 -0700

    Add project files.

---
+++ AuthenticationApp/Lib/SendGridAPI.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AuthenticationApp.Lib
+{
+    public class SendGridAPI
+    {
+        public static async Task<string> SendEmail(IdentityMessage message)
+        {
+
+            //String apiKey = Environment.GetEnvironmentVariable("SENDGRID_APIKEY", EnvironmentVariableTarget.User);
+            String apiKey = ConfigurationManager.AppSettings["SENDGRID_APIKEY"];
+
+            dynamic sg = new SendGridAPIClient(apiKey, "https://api.sendgrid.com");
+
+            Email From = new Email(ConfigurationManager.AppSettings["FROM_EMAIL"]);
+            Email To = new Email(message.Destination);
+            SendGrid.Helpers.Mail.Content content = new SendGrid.Helpers.Mail.Content("text/html", message.Body);
+            Mail mail = new Mail(From, message.Subject, To, content);
+
+            //mail.TemplateId = "13b8f94f-bcae-4ec6-b752-70d6cb59f932";
+            mail.Personalization[0].AddSubstitution("-name-", "www.lottotry.com");
+
+            try
+            {
+                dynamic response = await sg.client.mail.send.post(requestBody: mail.Get());
+                return response.StatusCode.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ AuthenticationApp/Lib/SmsApi.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Twilio;
+
+namespace AuthenticationApp.Lib
+{
+    public class SmsApi
+    {
+        public static Task SendSms(IdentityMessage message)
+        {
+#if true
+            var Twilio = new TwilioRestClient(ConfigurationManager.AppSettings["SMSAccountIdentification"],
+                                              ConfigurationManager.AppSettings["SMSAccountPassword"]);
+#else
+            var Twilio = new TwilioRestClient(Environment.GetEnvironmentVariable("SMSAccountIdentification", EnvironmentVariableTarget.User),
+                                             Environment.GetEnvironmentVariable("SMSAccountPassword", EnvironmentVariableTarget.User));
+#endif
+
+            var result = Twilio.SendMessage(ConfigurationManager.AppSettings["SMSAccountFrom"],
+                                            message.Destination, message.Body);
+
+            // Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+            Trace.TraceInformation(result.Status);
+
+            // Twilio doesn't currently have an async API, so return success.
+            return Task.FromResult(0);
+
+        }
+    }
+}
