9f171e2e0f2c364ef83f664d274a5808f8fa6087
Henry <henryma14@gmail.com>
2016-11-05 18:32:25 -0700

    Added landing page for customer

--- AuthenticationApp/Migrations_old/Configuration.cs
+++ AuthenticationApp/Migrations_old/Configuration.cs
@@ -5,14 +5,14 @@ namespace AuthenticationApp.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<AuthenticationApp.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<AuthenticationApp.Models.AppDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(AuthenticationApp.Models.ApplicationDbContext context)
+        protected override void Seed(AuthenticationApp.Models.AppDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
---
+++ AuthenticationApp/Models/TaskData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Temp2.Models
+{
+    public enum MATERIALS
+    {
+
+    }
+
+    public enum PROCESSES
+    {
+
+    }
+    public enum QUANTITY_BREAKS
+    {
+
+    }
+    public enum FILE_TYPE
+    {
+
+    }
+   
+
+
+    public class TaskData
+    {
+        [Key]
+        public Guid TaskId { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid VenderId { get; set; }
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+        [Required]
+        public int PartNumberRevision { get; set; }
+        [Required]
+        public MATERIALS Material { get; set; }
+        [Required]
+        public PROCESSES Process { get; set; }
+        [Required]
+        public QUANTITY_BREAKS QuantityBreak { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime LeadTime { get; set; }
+        [Required]
+        public FILE_TYPE FileType { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreateTC { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime UpdateTC { get; set; }
+    }
+}
